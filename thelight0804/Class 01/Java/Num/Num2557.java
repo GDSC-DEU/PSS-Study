0773b193dbc5eaabc769cb67f104d41a8eacfb9c
thelight0804 <thelight0804@gmail.com>
2022-05-11 23:48:02 +0900

    Add base Files

---
+++ thelight0804/Class 01/Java/Num/Num2557.java
@@ -0,0 +1,15 @@
+/*
+Date : 22.04.27
+Title : BAEKJOON 2557번
+Project : Hello World
+Name : thelight0804
+제출 시 class 이름을 Main으로 변경해야 한다
+*/
+
+package Num;
+
+public class Num2557 {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ thelight0804/Class 01/Python/num_1000.py
@@ -0,0 +1,10 @@
+"""
+Date : 22.03.31
+Title : BAEKJOON 1000번
+Project : A+B
+Name : thelight0804
+"""
+num = input()
+a = int(num[0])
+b = int(num[2])
+print(a+b)
